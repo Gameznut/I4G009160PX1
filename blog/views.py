80c5cc17612721e0ad26cbc723f135b327dc2a84
emmanuel apabiekun <31988283+Gameznut@users.noreply.github.com>
2022-06-29 14:22:42 +0100

    Commit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy, __all__
 
-# Create your views here.
+import blog
+from blog.models import Post
+
+
+class PostListView:
+    model = Post
+
+
+class PostCreateView:
+    model = Post
+    fields = __all__
+    success_url = reverse_lazy(blog)
+
+
+class PostDetailView:
+    model = Post
+
+
+class PostUpdateView:
+    model = Post
+    fields = __all__
+    success_url = reverse_lazy(blog)
+
+
+class PostDeleteView:
+    model = Post
+    fields = __all__
+    success_url = reverse_lazy(blog)
